6516646e3913c9e9f338ba4bc889d409596a2c1d
Hester <wenxuan1031838@gmail.com>
2023-05-11 03:57:34 +0800

    add weather model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,13 +1,16 @@
 import 'package:tmwes/models/current_weather_model.dart';
+import 'package:tmwes/models/daily_weather_model.dart';
 import 'package:tmwes/models/hourly_weather_model.dart';
 
 class WeatherModel {
   final CurrentWeatherModel? current;
   final HourlyWeatherModel? hourly;
+  final DailyWeatherModel? daily;
 
-  WeatherModel([this.current, this.hourly]);
+  WeatherModel([this.current, this.hourly, this.daily]);
 
   //fetch value
   CurrentWeatherModel getCurrentWeather() => current!;
   HourlyWeatherModel getHourlyWeather() => hourly!;
+  DailyWeatherModel getDailyWeather() => daily!;
 }
