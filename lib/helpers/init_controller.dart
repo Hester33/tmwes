dea8a52d4a57de490619f1c6bc52bd58563c3fbe
Hester <wenxuan1031838@gmail.com>
2023-05-26 01:48:32 +0800

    initialize hit 6 controller

--- lib/helpers/init_controller.dart
+++ lib/helpers/init_controller.dart
@@ -5,6 +5,7 @@ import 'package:tmwes/controllers/login_controller.dart';
 import 'package:tmwes/controllers/profile_controller.dart';
 import 'package:tmwes/controllers/signup_controller.dart';
 import 'package:tmwes/services/authentication_db.dart';
+import 'package:tmwes/services/hit6_db.dart';
 import 'package:tmwes/services/user_db.dart';
 import 'package:tmwes/widgets/animation/controller_fade_in_aniamtion.dart';
 
@@ -19,6 +20,7 @@ class ControllerBindings extends Bindings {
     Get.lazyPut<SignUpController>(() => SignUpController(), fenix: true);
     Get.lazyPut<ProfileController>(() => ProfileController(), fenix: true);
     Get.lazyPut<HomeController>(() => HomeController(), fenix: true);
+    Get.lazyPut<HIT6Db>(() => HIT6Db(), fenix: true);
     Get.lazyPut<HIT6Controller>(() => HIT6Controller(), fenix: true);
   }
 }
