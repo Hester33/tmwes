52c08b9f25f692d0ff568fffb3b94240365d79b5
Hester <wenxuan1031838@gmail.com>
2023-05-31 21:49:40 +0800

    add date format

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:tmwes/constants/colors.dart';
 import 'package:tmwes/services/authentication_db.dart';
 import 'package:tmwes/services/user_db.dart';
@@ -15,7 +16,9 @@ import 'helpers/init_controller.dart';
 
 //!flutter pub upgrade --major-version
 
-void main() {
+void main() async {
+  // Initialize date formatting for the 'ms_MY' locale (Malay language in Malaysia)
+  await initializeDateFormatting('en_MY');
   WidgetsFlutterBinding.ensureInitialized();
   //hide system bottom navigation bar
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky,
