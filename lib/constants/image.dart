6bf2da5daaf7457e84f0362945155d94eb7e4af8
Hester <wenxuan1031838@gmail.com>
2023-06-03 17:20:22 +0800

    add record migraine icon

--- lib/constants/image.dart
+++ lib/constants/image.dart
@@ -11,6 +11,8 @@ final String pressureImg = _getImagePath("weather/pressure.png");
 //record migraine
 final String painLvlIcon = _getImagePath("images/lightning-bolt.png");
 final String painPositionIcon = _getImagePath("images/pain-position.png");
+final String triggerIcon = _getImagePath("images/triggers.png");
+final String medicineIcon = _getImagePath("images/medicine.png");
 
 String _getImagePath(String fileName) {
   return imageAssetsRoot + fileName;
