47e7994d8272fac06d1157a6881f20b2587be777
Hester <wenxuan1031838@gmail.com>
2023-06-30 00:10:45 +0800

    change onecall API3.0 to 2.5

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -3,6 +3,14 @@ import 'package:tmwes/api/api_key.dart';
 String apiUrl(var lat, var lon) {
   String url;
   url =
-      "https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely";
+      //"https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely";
+      "https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely";
+  return url;
+}
+
+String apiUrlHistory(var lat, var lon, var dt) {
+  String url;
+  url =
+      "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=$lat&lon=$lon&dt=$dt&appid=$apiKey&only_current={true}";
   return url;
 }
